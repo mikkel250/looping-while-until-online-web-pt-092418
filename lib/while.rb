850613ba0b76fa2acfee321970fcf1bde811b85d
Mikkel Ridley <mikkel250@gmail.com>
2018-09-25 06:08:46 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
-def using_until
-        levitation_force = 6
-        #your code here
-        until levitation_force == 10
-        puts "Wingardium Leviosa"
-        levitation_force += 1
-      end
-    
+def using_while
+	levitation_force = 6
+	
+	#your code here
+	while levitation_force < 10
+	puts "Wingardium Leviosa"
+	levitation_force += 1
+	end
 end
